bdd3c1266f403df9454d7ab7d4dbc0d7639f0d0a
bodalciouz <carlayrtoncrespo7205@gmail.com>
2025-11-16 00:07:20 +0800

    updated occupancy report and dao

--- OccupancyReport.java
+++ OccupancyReport.java
@@ -3,12 +3,14 @@ public class OccupancyReport {
     private String lockerSize;
     private String lockerLocation;
     private int totalBookings;
+    private double occupancyPercentage;
 
-    public OccupancyReport(int lockerID, String lockerSize, String lockerLocation, int totalBookings) {
+    public OccupancyReport(int lockerID, String lockerSize, String lockerLocation, int totalBookings, double occupancyPercentage) {
         this.lockerID = lockerID;
         this.lockerSize = lockerSize;
         this.lockerLocation = lockerLocation;
         this.totalBookings = totalBookings;
+        this.occupancyPercentage = occupancyPercentage;
     }
 
     public int getLockerID() { 
@@ -16,14 +18,23 @@ public class OccupancyReport {
     }
 
     public String getLockerSize() { 
-        return lockerSize;
+        return lockerSize; 
     }
 
     public String getLockerLocation() { 
-        return lockerLocation;
+        return lockerLocation; 
     }
 
     public int getTotalBookings() { 
-        return totalBookings;
+        return totalBookings; 
+    }
+
+    public double getOccupancyPercentage() { 
+        return occupancyPercentage; 
+    }
+
+    @Override
+    public String toString() {
+        return lockerID + " | " + lockerSize + " | " + lockerLocation + " | " + totalBookings + " | " + String.format("%.2f", occupancyPercentage) + "%";
     }
 }
--- OccupancyReportDAO.java
+++ OccupancyReportDAO.java
@@ -15,14 +15,14 @@ public class OccupancyReportDAO {
     public List<OccupancyReport> getMonthlyOccupancy(int month, int year) {
         List<OccupancyReport> list = new ArrayList<>();
 
-        String sql = "SELECT L.lockerID, LT.lockerTypeSize, LOC.locationName, COUNT(B.bookingReference) AS totalBookings " +
-                     "FROM Locker L " +
-                     "JOIN LockerType LT ON L.lockerTypeID = LT.lockerTypeID " +
-                     "JOIN Location LOC ON L.locationID = LOC.locationID " +
-                     "LEFT JOIN Booking B ON L.lockerID = B.lockerID " +
-                     "AND MONTH(B.bookingDate) = ? AND YEAR(B.bookingDate) = ? " +
-                     "GROUP BY L.lockerID, LT.lockerTypeSize, LOC.locationName " +
-                     "ORDER BY L.lockerID";
+        String sql =
+            "SELECT L.lockerID, LT.lockerTypeSize, LOC.locationName, COUNT(B.bookingReference) AS totalBookings " +
+            "FROM Locker L " +
+            "JOIN LockerType LT ON L.lockerTypeID = LT.lockerTypeID " +
+            "JOIN Location LOC ON L.locationID = LOC.locationID " +
+            "LEFT JOIN Booking B ON L.lockerID = B.lockerID AND MONTH(B.reservationDate) = ? AND YEAR(B.reservationDate) = ? " +
+            "GROUP BY L.lockerID, LT.lockerTypeSize, LOC.locationName " +
+            "ORDER BY LOC.locationName, L.lockerID";
 
         try (Connection conn = getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
@@ -31,14 +31,21 @@ public class OccupancyReportDAO {
             stmt.setInt(2, year);
 
             ResultSet rs = stmt.executeQuery();
+            int daysInMonth = java.time.YearMonth.of(year, month).lengthOfMonth();
+
             while (rs.next()) {
+                int totalBookings = rs.getInt("totalBookings");
+                double occupancyPct = ((double) totalBookings / daysInMonth) * 100;
+
                 list.add(new OccupancyReport(
                         rs.getInt("lockerID"),
                         rs.getString("lockerTypeSize"),
                         rs.getString("locationName"),
-                        rs.getInt("totalBookings")
+                        totalBookings,
+                        occupancyPct
                 ));
             }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
